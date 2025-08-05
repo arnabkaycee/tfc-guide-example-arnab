fef27e7cd7c4d13f2196e9e85c1533d7fe39e81c
Arnab Chatterjee <arnabkaycee@gmail.com>
2025-08-05 17:20:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "region" {
   description = "AWS region"
   default     = "us-west-1"
 }
+variable "s3_bucket_name" {
+  description = "Name of bucket"
+  default = "foo-bar-bucket-tfc-guide-example-arnab"
+}
