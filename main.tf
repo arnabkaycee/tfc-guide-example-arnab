302a7d3381663668dde420595e5ee05995c850a4
Arnab Chatterjee <arnabkaycee@gmail.com>
2025-08-05 17:18:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,27 +5,12 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket" "example" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
 
   tags = {
-    Name = var.instance_name
+    Name        = var.s3_bucket_name
+    Environment = "Dev"
   }
 }
